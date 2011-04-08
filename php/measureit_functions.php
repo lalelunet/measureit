cf4d69f4dc7ef22c0552210c91b3e13dd34f16eb
Thomas E <lalelunet@gmail.com>
2011-04-08 18:27:14 +0000

    if a new position from the sensor was added the start screen uses consumptions from the latest  position too

--- php/measureit_functions.php
+++ php/measureit_functions.php
@@ -73,13 +73,14 @@ function sensor_detail( $params = array( ) ){
 function summary_start( ){
 	$sensors = sensor_get();
 	foreach( $sensors as $k=>$v ){
+		$p = end( $v['positions'] );
 		$r[$k]['sensor'] = $v;
 		$r[$k]['tmpr'] = sensor_item_get( array( 'sensor'=> $k, 'table'=> 'measure_tmpr', 'timeframe'=> 'last') );
 		$r[$k]['watt'] = sensor_item_get( array( 'sensor'=> $k, 'table'=> 'measure_watt', 'timeframe'=> 'last') );
-		$r[$k]['daily'] = sensor_item_get( array( 'sensor'=> $k, 'table'=> 'measure_watt_daily', 'timeframe'=> 'last'  ) );
-		$r[$k]['hourly'] = sensor_item_get( array( 'sensor'=> $k, 'table'=> 'measure_watt_hourly', 'timeframe'=> 'last', 'order' => 'hour'  ) );
-		$r[$k]['weekly'] = price_sum( sensor_data_raw_get( array( 'sensor'=> $k, 'unit_value'=> 7, 'unit'=> 'day', 'table'=> 'measure_watt_daily', 'timeframe'=> 'static'  ) ) );
-		$r[$k]['monthly'] = price_sum( sensor_data_raw_get( array( 'sensor'=> $k, 'unit_value'=> 30, 'unit'=> 'day', 'table'=> 'measure_watt_daily', 'timeframe'=> 'static'  ) ) );
+		$r[$k]['daily'] = sensor_item_get( array( 'sensor'=> $k, 'table'=> 'measure_watt_daily', 'timeframe'=> 'last', 'limit' => $p['time']  ) );
+		$r[$k]['hourly'] = sensor_item_get( array( 'sensor'=> $k, 'table'=> 'measure_watt_hourly', 'timeframe'=> 'last', 'order' => 'hour', 'limit' => $p['time']  ) );
+		$r[$k]['weekly'] = price_sum( sensor_data_raw_get( array( 'sensor'=> $k, 'unit_value'=> 7, 'unit'=> 'day', 'table'=> 'measure_watt_daily', 'timeframe'=> 'static', 'limit' => $p['time']  ) ) );
+		$r[$k]['monthly'] = price_sum( sensor_data_raw_get( array( 'sensor'=> $k, 'unit_value'=> 30, 'unit'=> 'day', 'table'=> 'measure_watt_daily', 'timeframe'=> 'static', 'limit' => $p['time']  ) ) );
 	}
 	print json_encode($r);
 	return true;
@@ -156,7 +157,8 @@ function data_query_build( $params = array( ) ){
 		case 'static':
 			$unit = preg_match( '/(hour|day| month)/i', $params['unit'] ) ? $params['unit'] : error( $params['unit'] );
 			$unit_value = is_numeric( $params['unit_value'] ) ? $params['unit_value'] : error( $params['unit_value'] );
-			$timeframe = " AND time > NOW( ) - INTERVAL $unit_value $unit  ORDER BY $order";
+			$limit = isset( $params['limit'] ) ? ' AND time > "'.$params['limit'].'"' : '';
+			$timeframe = " AND time > NOW( ) - INTERVAL $unit_value $unit  $limit ORDER BY $order";
 		break;
 		case 'last':
 			$timeframe = " ORDER BY $order DESC LIMIT 1";
@@ -179,7 +181,7 @@ function data_query_build( $params = array( ) ){
 		break;
 	}
 	$query = "SELECT * FROM $table WHERE sensor = '$sensor' $timeframe";
-	#print "SELECT * FROM $table WHERE sensor = '$sensor' $timeframe";exit;
+	#print "SELECT * FROM $table WHERE sensor = '$sensor' $timeframe<br />";
 	return $query;
 }
 
