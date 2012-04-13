8732ab85c3da69612d68964ecc98e9b248e5b370
Thomas E <lalelunet@gmail.com>
2012-04-13 16:15:58 +0000

    reduce python code to one file that works on win and linux

--- python/data-input.py
+++ python/data-input.py
@@ -4,44 +4,143 @@ import MySQLdb
 import datetime
 import warnings
 import time
+import threading
+import sys
+import platform
+
+sensors = {}
+settings = {}
+db = {}
+
+usbport = 'COM3'
+if platform.system() == 'Linux':
+    usbport = '/dev/ttyUSB0'
 
 warnings.filterwarnings("ignore")
-
 # mysql connection
-mysql = MySQLdb.connect("localhost","measureit","measureitpasswd","measure_it" )
+#mysql = MySQLdb.connect(host="10.0.0.12",port=10001,user="measureit",passwd="nhjw_4k=0)/_rhje$$/e34%",db="measure_it" )
+mysql = MySQLdb.connect(host="localhost",port=3306,user="measureit",passwd="measureitpasswd",db="measure_it" )
 db = mysql.cursor()
-# com connection
-ser = serial.Serial(port='COM3', baudrate=57600, bytesize=serial.EIGHTBITS, parity=serial.PARITY_NONE, stopbits=serial.STOPBITS_ONE, timeout=3)
 
+def sensor_list_get():
+    db.execute('SELECT sensor_id FROM measure_sensors')
+    r = db.fetchall()
+    for row in r:
+        sensor = int(row[0])
+        sensors[sensor] = {'tmpr' : 0, 'watt' : 0}
+    return sensors
 
-	
+def sensor_settings_get():
+    sensors_settings = {}
+    db.execute('SELECT sensor_id, measure_history FROM measure_sensors LEFT JOIN measure_settings ON measure_settings.measure_sensor = measure_sensors.sensor_id')
+    r = db.fetchall()
+    for row in r:
+        sensor = int(row[0])
+        sensors_settings[sensor] = row[1]
+    return sensors_settings
+
+def system_settings_get():
+    system_settings = {}
+    db.execute('SELECT measure_system_setting_name, measure_system_setting_value FROM measure_system')
+    r = db.fetchall()
+    return r
+
+def sensor_data_change( coloum, sensor, data):
+    sensor = str(sensor)
+    data = str(data)
+    db.execute('UPDATE measure_data_now SET '+coloum+' = "'+data+'" WHERE sensor_id = '+sensor)
+
+def sensor_watt_insert( sensor, watt ):
+    sensor = str(sensor)
+    watt = str(watt)
+    db.execute('INSERT INTO measure_watt ( sensor, data, time) values( '+sensor+', '+watt+', NOW() ) ')
+
+def tmpr_insert( tmpr ):
+    now = datetime.datetime.now()
+    db.execute('INSERT INTO measure_tmpr ( data, time ) values( "'+str(tmpr)+'", NOW() ) ')
+    db.execute('INSERT IGNORE INTO measure_tmpr_hourly ( data, time, hour ) VALUES ( "'+str(tmpr)+'", NOW(), "'+str(now.hour)+'" )')
+    
+def history_update( sensor, hist ):
+    date_hour = date_hour_get(hist[1])
+    if hist[0] == 'm':
+        query = 'INSERT IGNORE INTO measure_watt_monthly ( sensor, data, time ) VALUES ( "'+str(sensor)+'", "'+str(hist[2])+'", "'+date_hour[0]+'" )'
+    if hist[0] == 'd':
+        query = 'INSERT IGNORE INTO measure_watt_daily ( sensor, data, time ) VALUES ( "'+str(sensor)+'", "'+str(hist[2])+'", "'+date_hour[0]+'" )'
+    if hist[0] == 'h':
+        query = 'INSERT IGNORE INTO measure_watt_hourly ( sensor, data, hour, time ) VALUES ( "'+str(sensor)+'", "'+str(hist[2])+'", "'+date_hour[1]+'", "'+date_hour[0]+'" )'
+    db.execute(query)
+
+def date_hour_get( hours ):
+    db.execute('SELECT NOW() - INTERVAL '+hours+' HOUR')
+    date = db.fetchone()
+    r = re.search(r"(\d+-\d+-\d+) (\d+):.+", str(date[0]) )
+    return (r.group(1), r.group(2))
+
+def sensor_data_check( sensor, watt, tmpr ):
+    sensor = int(sensor)
+    watt = int(watt)
+    if sensors.has_key(sensor):
+        if sensors[sensor]['tmpr'] != tmpr:
+            sensors[sensor]['tmpr'] = tmpr
+            sensor_data_change( 'tmpr', sensor, tmpr )
+            tmpr_insert( tmpr )
+        if sensors[sensor]['watt'] != watt:
+            sensors[sensor]['watt'] = watt
+            sensor_data_change( 'watt', sensor, watt )
+            sensor_watt_insert( sensor, watt )
+
+def cron_timer_hourly():
+    for sensor in sensors:
+        print sensor
+    t = threading.Timer(10.0, cron_timer)
+    t.start()
+
 sensors = sensor_list_get()
+#cron_timer()
 
-print 'Data grabbing is startet. Leave this window open to get permanet data from your device. In a few moments you should see your current watt usage'
-time.sleep(3)
-														 
+ser = serial.Serial(port=usbport, baudrate=57600, bytesize=serial.EIGHTBITS, parity=serial.PARITY_NONE, stopbits=serial.STOPBITS_ONE, timeout=3)
+
+if 'test' in sys.argv:
+    debug = True
+
 while True:
-	line = ser.readline()
-	line = line.rstrip('\r\n')
-	r = re.search(r"<hist>", line)
-	if r:
-		for s in sensors:
-			r = re.search(r"<data><sensor>"+str(s)+"</sensor>(.+?)</data>", line)
-			if r:
-				d = re.findall(r"<(d|m|h)(\d+)>(.+?)</.+?>", r.group(1) )
-				if d:
-					for f in d:
-						history_update(s,f)
-				
-	r = re.search(r"<tmpr>(.+?)</tmpr><sensor>(\d+)</sensor>.*<watts>(\d+)</watts>", line)
-	if r: 
-		if sensors[int( r.group(2) )]['tmpr'] != r.group(1):
-				sensors[int( r.group(2) )]['tmpr'] = r.group(1)
-				sensor_data_change( 'tmpr', r.group(2), r.group(1) )
-				tmpr_insert( r.group(1) )
-		if sensors[int( r.group(2) )]['watt'] != r.group(1):
-				sensors[int( r.group(2) )]['watt'] = r.group(3)
-				sensor_data_change( 'watt', r.group(2), r.group(3) )
-				sensor_watt_insert( r.group(2), r.group(3) )	
-		
-		print 'Sensor '+r.group(2)+' Watt '+r.group(3)
+    line = ser.readline()
+    line = line.rstrip('\r\n')
+    clamps = False
+    r = re.search(r"<hist>", line)
+    if r:
+        for s in sensors:
+            r = re.search(r"<data><sensor>"+str(s)+"</sensor>(.+?)</data>", line)
+            if r:
+                d = re.findall(r"<(d|m|h)(\d+)>(.+?)</.+?>", r.group(1) )
+                if d:
+                    for f in d:
+                        history_update(s,f)
+    r = re.search(r"<tmpr>(.+?)</tmpr><sensor>(\d)+</sensor>.+<ch1><watts>(\d+)<\/watts><\/ch1>(<ch2><watts>(\d+)<\/watts><\/ch2><ch3><watts>(\d+)<\/watts><\/ch3>)?", line)
+    if r:
+        tmpr = r.group(1)
+        watt_sum = int(r.group(3))
+        # more than 1 clamp
+        if r.group(5):
+            sensor = '2'+r.group(2)
+            watt = int(r.group(5))
+            watt_sum += watt
+            sensor_data_check( sensor, watt, tmpr )
+            clamps = True
+        if r.group(6):
+            sensor = '3'+r.group(2)
+            watt = int(r.group(6))
+            watt_sum += watt
+            sensor_data_check( sensor, watt, tmpr )
+            clamps = True
+            
+        if clamps:
+            sensor = '1'+r.group(2)
+            watt = int(r.group(3))
+            sensor_data_check( sensor, watt, tmpr )
+           
+        sensor_data_check( r.group(2), watt_sum, tmpr )
+        
+        
+
+
