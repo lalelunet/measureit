8d8df6b8763d89fa40980cc39ff2a3f149d8bace
Thomas E <lalelunet@gmail.com>
2012-06-18 05:43:13 +0000

    new create sql

--- install/createdb.sql
+++ install/createdb.sql
@@ -5,117 +5,109 @@ CREATE USER 'measureit'@'localhost' IDENTIFIED BY 'measureitpasswd';
 
 GRANT SELECT , INSERT , UPDATE , DELETE, LOCK TABLES ON * . * TO 'measureit'@'localhost' IDENTIFIED BY 'measureitpasswd';
 
-CREATE TABLE IF NOT EXISTS measure_positions (
-  position_id int(11) NOT NULL AUTO_INCREMENT,
-  position_time datetime NOT NULL,
-  position_description varchar(128) NOT NULL,
-  position_sensor tinyint(1) NOT NULL,
-  PRIMARY KEY (position_id)
-) ENGINE=MyISAM  DEFAULT CHARSET=latin1;
+CREATE TABLE IF NOT EXISTS `measure_costs` (
+  `costs_id` int(11) NOT NULL AUTO_INCREMENT,
+  `costs_sensor` smallint(3) NOT NULL DEFAULT '100',
+  `costs_from` tinyint(2) NOT NULL,
+  `costs_to` tinyint(2) NOT NULL,
+  `costs_price` float NOT NULL,
+  `costs_since` date NOT NULL,
+  PRIMARY KEY (`costs_id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1;
 
-
-CREATE TABLE IF NOT EXISTS measure_sensors (
-  sensor_id smallint(5) NOT NULL,
-  sensor_title varchar(100) NOT NULL,
-  PRIMARY KEY (sensor_id),
-  UNIQUE KEY sensor_id (sensor_id)
-) ENGINE=MyISAM  DEFAULT CHARSET=latin1;
-
-
-CREATE TABLE IF NOT EXISTS measure_settings (
-  measure_history smallint(6) NOT NULL DEFAULT '365',
-  measure_currency varchar(5) NOT NULL DEFAULT 'Pound',
-  measure_sensor tinyint(5) NOT NULL,
-  measure_price float NOT NULL DEFAULT '0.0021',
-  measure_range varchar(5) NOT NULL,
-  measure_timeframe smallint(4) NOT NULL,
-  measure_timezone varchar(128) NOT NULL DEFAULT 'GMT0',
-  measure_timezone_diff smallint(4) NOT NULL DEFAULT '0'
+CREATE TABLE IF NOT EXISTS `measure_data_now` (
+  `sensor_id` tinyint(2) NOT NULL,
+  `watt` smallint(5) NOT NULL,
+  `tmpr` float NOT NULL,
+  PRIMARY KEY (`sensor_id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
-
-CREATE TABLE IF NOT EXISTS measure_tmpr (
-  tmpr_id int(128) NOT NULL AUTO_INCREMENT,
-  data float NOT NULL,
-  time datetime NOT NULL,
-  PRIMARY KEY (tmpr_id)
+CREATE TABLE IF NOT EXISTS `measure_positions` (
+  `position_id` int(11) NOT NULL AUTO_INCREMENT,
+  `position_time` datetime NOT NULL,
+  `position_description` varchar(128) NOT NULL,
+  `position_sensor` tinyint(3) NOT NULL,
+  PRIMARY KEY (`position_id`)
 ) ENGINE=MyISAM  DEFAULT CHARSET=latin1;
 
-
-CREATE TABLE IF NOT EXISTS measure_tmpr_hourly (
-  hour_id int(11) NOT NULL AUTO_INCREMENT,
-  data float NOT NULL,
-  hour tinyint(2) NOT NULL,
-  time date NOT NULL,
-  PRIMARY KEY (hour_id),
-  UNIQUE KEY data (hour,time)
+CREATE TABLE IF NOT EXISTS `measure_sensors` (
+  `sensor_id` smallint(5) NOT NULL,
+  `sensor_title` varchar(100) NOT NULL,
+  `sensor_clamp` tinyint(1) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`sensor_id`),
+  UNIQUE KEY `sensor_id` (`sensor_id`)
 ) ENGINE=MyISAM  DEFAULT CHARSET=latin1;
 
-
-CREATE TABLE IF NOT EXISTS measure_watt (
-  watt_id int(128) NOT NULL AUTO_INCREMENT,
-  sensor tinyint(5) NOT NULL,
-  data smallint(5) NOT NULL,
-  time datetime NOT NULL,
-  PRIMARY KEY (watt_id),
-  KEY time_sensor (time,sensor)
-) ENGINE=MyISAM  DEFAULT CHARSET=latin1;
-
-
-CREATE TABLE IF NOT EXISTS measure_watt_daily (
-  day_id int(11) NOT NULL AUTO_INCREMENT,
-  sensor tinyint(5) NOT NULL,
-  data float NOT NULL,
-  time date NOT NULL,
-  PRIMARY KEY (day_id),
-  UNIQUE KEY data (sensor,time)
-) ENGINE=MyISAM  DEFAULT CHARSET=latin1;
-
-
-CREATE TABLE IF NOT EXISTS measure_watt_hourly (
-  hour_id int(11) NOT NULL AUTO_INCREMENT,
-  sensor tinyint(5) NOT NULL,
-  data float NOT NULL,
-  hour tinyint(2) NOT NULL,
-  time date NOT NULL,
-  PRIMARY KEY (hour_id),
-  UNIQUE KEY data (sensor,hour,time)
-) ENGINE=MyISAM  DEFAULT CHARSET=latin1;
-
-
-CREATE TABLE IF NOT EXISTS measure_watt_monthly (
-  month_id int(11) NOT NULL AUTO_INCREMENT,
-  sensor tinyint(5) NOT NULL,
-  data float NOT NULL,
-  time date NOT NULL,
-  PRIMARY KEY (month_id),
-  UNIQUE KEY data (sensor,time)
+CREATE TABLE IF NOT EXISTS `measure_settings` (
+  `measure_history` smallint(6) NOT NULL DEFAULT '365',
+  `measure_currency` varchar(5) NOT NULL DEFAULT 'Pound',
+  `measure_sensor` tinyint(5) NOT NULL,
+  `measure_range` varchar(5) NOT NULL,
+  `measure_timeframe` smallint(4) NOT NULL,
+  `measure_timezone` varchar(128) NOT NULL DEFAULT 'GMT0',
+  `measure_timezone_diff` smallint(4) NOT NULL DEFAULT '0'
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
-CREATE TABLE IF NOT EXISTS measure_data_now (
-  sensor_id tinyint(2) NOT NULL,
-  watt smallint(5) NOT NULL,
-  tmpr float NOT NULL,
-  PRIMARY KEY (sensor_id)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+CREATE TABLE IF NOT EXISTS `measure_system` (
+  `measure_system_setting_id` int(11) NOT NULL AUTO_INCREMENT,
+  `measure_system_setting_name` varchar(256) NOT NULL,
+  `measure_system_setting_value` varchar(256) NOT NULL,
+  PRIMARY KEY (`measure_system_setting_id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1;
 
-CREATE TABLE IF NOT EXISTS measure_system (
-  measure_system_setting_id int(11) NOT NULL AUTO_INCREMENT,
-  measure_system_setting_name varchar(256) NOT NULL,
-  measure_system_setting_value varchar(256) NOT NULL,
-  PRIMARY KEY (measure_system_setting_id)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1;
+CREATE TABLE IF NOT EXISTS `measure_tmpr` (
+  `tmpr_id` int(128) NOT NULL AUTO_INCREMENT,
+  `data` float NOT NULL,
+  `time` datetime NOT NULL,
+  PRIMARY KEY (`tmpr_id`)
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1;
 
-CREATE TABLE IF NOT EXISTS measure_costs (
-  costs_id int(11) NOT NULL AUTO_INCREMENT,
-  costs_sensor smallint(3) NOT NULL DEFAULT '100',
-  costs_from tinyint(2) NOT NULL,
-  costs_to tinyint(2) NOT NULL,
-  costs_price float NOT NULL,
-  costs_since date NOT NULL,
-  PRIMARY KEY (costs_id)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=1;
+CREATE TABLE IF NOT EXISTS `measure_tmpr_hourly` (
+  `hour_id` int(11) NOT NULL AUTO_INCREMENT,
+  `data` float NOT NULL,
+  `hour` tinyint(2) NOT NULL,
+  `time` date NOT NULL,
+  PRIMARY KEY (`hour_id`),
+  UNIQUE KEY `data` (`hour`,`time`)
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1;
 
+CREATE TABLE IF NOT EXISTS `measure_watt` (
+  `watt_id` int(128) NOT NULL AUTO_INCREMENT,
+  `sensor` tinyint(3) NOT NULL,
+  `data` smallint(5) NOT NULL,
+  `time` datetime NOT NULL,
+  PRIMARY KEY (`watt_id`),
+  KEY `time_sensor` (`time`,`sensor`)
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1;
+
+CREATE TABLE IF NOT EXISTS `measure_watt_daily` (
+  `day_id` int(11) NOT NULL AUTO_INCREMENT,
+  `sensor` tinyint(1) NOT NULL,
+  `data` float NOT NULL,
+  `time` date NOT NULL,
+  PRIMARY KEY (`day_id`),
+  UNIQUE KEY `data` (`sensor`,`time`),
+  KEY `time_sensor` (`time`,`sensor`)
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1;
+
+CREATE TABLE IF NOT EXISTS `measure_watt_hourly` (
+  `hour_id` int(11) NOT NULL AUTO_INCREMENT,
+  `sensor` tinyint(1) NOT NULL,
+  `data` float NOT NULL,
+  `hour` tinyint(2) NOT NULL,
+  `time` date NOT NULL,
+  PRIMARY KEY (`hour_id`),
+  UNIQUE KEY `data` (`sensor`,`hour`,`time`)
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1;
+
+CREATE TABLE IF NOT EXISTS `measure_watt_monthly` (
+  `month_id` int(11) NOT NULL AUTO_INCREMENT,
+  `sensor` tinyint(1) NOT NULL,
+  `data` float NOT NULL,
+  `time` date NOT NULL,
+  PRIMARY KEY (`month_id`),
+  UNIQUE KEY `data` (`sensor`,`time`)
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1;
 
 INSERT INTO measure_data_now (sensor_id, watt, tmpr) VALUES
 (0, 0, 0),
@@ -130,4 +122,4 @@ INSERT INTO measure_data_now (sensor_id, watt, tmpr) VALUES
 
 INSERT INTO measure_it.measure_positions (position_time, position_description, position_sensor) VALUES (now(), 'start position', '0');
 INSERT INTO measure_it.measure_sensors (sensor_id, sensor_title) VALUES ('0', 'Sensor 0');
-INSERT INTO measure_it.measure_settings (measure_history, measure_currency, measure_sensor, measure_price) VALUES ('365', 'Euro', '0', '0.0022');
+INSERT INTO measure_it.measure_settings (measure_history, measure_currency, measure_sensor) VALUES ('365', 'Euro', '0');
