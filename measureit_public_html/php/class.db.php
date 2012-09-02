996af0561306c6c3f81f1459ad890082f87143b0
Thomas E <lalelunet@gmail.com>
2012-09-02 10:01:02 +0000

    cleaning code and usage of a shared config file

--- measureit_public_html/php/class.db.php
+++ measureit_public_html/php/class.db.php
@@ -1,60 +1,47 @@
 <?php
+require_once '/usr/local/measureit/measureit.cfg.php';
 
- class db_accountdata {
-  var $host                 = "localhost";
-  var $user                 = "measureit";
-  var $pass                 = "measureitpasswd";
-  var $datenbank            = "measure_it";
-}
+class mydb{
 
+	function mydb(){
+		global $database_host, $database_name, $database_port, $database_user, $database_passwd;
+		$this->connect($database_host.':'.$database_port,$database_user,$database_passwd,$database_name);
+	}
 
-class mydb extends db_accountdata
-{
+	function connect($host,$user,$pass,$datenbank){
+		$this->link = mysql_connect($host,$user,$pass) or die ("Datenbankverbindung nicht moeglich!");
+		$this->choosedb($datenbank);
+	}
 
-   function mydb()
-   {
-      $this->connect($this->host,$this->user,$this->pass,$this->datenbank);
-   }
+	function choosedb($datenbank){
+		mysql_select_db($datenbank) or die ("Datenbank konnte nicht ausgewaehlt werden!");
+	}
 
-   function connect($host,$user,$pass,$datenbank)
-   {
-      $this->link = @mysql_connect($host,$user,$pass) or die ("Datenbankverbindung nicht moeglich!");
-      $this->choosedb($datenbank);
-   }
+	function query($query){
+		$res = mysql_query($query, $this->link) or die ("SQL Abfrage ist ungueltig. ".mysql_error());
+		return $res;
+	}
 
-   function choosedb($datenbank)
-   {
-      @mysql_select_db($datenbank) or die ("Datenbank konnte nicht ausgewaehlt werden!");
-   }
+	function fetch_array($res){
+		return mysql_fetch_array($res);
+	}
 
-   function query($query)
-   {
-      $res = @mysql_query($query, $this->link) or die ("SQL Abfrage ist ungueltig. ".mysql_error());
-      return $res;
-   }
+	function fetch_row($res){
+		return mysql_fetch_row($res);
+	}
 
-   function fetch_array($res)
-   {
-     return mysql_fetch_array($res);
-   }
-
-   function fetch_row($res)
-   {
-     return mysql_fetch_row($res);
-   }
-
-   function num_rows($res)
-   {
-     return mysql_num_rows($res);
-   }
-   function insert_id()
-   {
-     return mysql_insert_id();
-   }
-   function backup(){
-   		set_time_limit(0);
-		system( 'mysqldump --opt -h'.$this->host.' -u'.$this->user.' -p'.$this->pass.' '.$this->datenbank.' | gzip > ../backup/measureit_backup_'.date('Ymd-His').'.gz' );
-   }
+	function num_rows($res){
+		return mysql_num_rows($res);
+	}
+   
+	function insert_id(){
+		return mysql_insert_id();
+	}
+	
+	function backup(){
+		set_time_limit(0);
+		system( 'mysqldump --opt -h'.$database_host.':'.$database_port.' -u'.$database_user.' -p'.$database_passwd.' '.$database_name.' | gzip > ../backup/measureit_backup_'.date('Ymd-His').'.gz' );
+	}
 
 }
 
