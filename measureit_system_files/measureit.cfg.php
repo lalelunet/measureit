69e9877a1d327f24a347f9ea3b4c662400f3273d
Thomas E <lalelunet@gmail.com>
2012-09-02 10:05:03 +0000

    use a global shared config for all scripts

---
+++ measureit_system_files/measureit.cfg.php
@@ -0,0 +1,17 @@
+<?php
+
+# This config file is parsed from the data grabber script too.
+# Please make shure that the format is always $variable = 'value';
+
+	$database_host = 'localhost';
+	$database_port = '3306';
+	$database_name = 'measure_it';
+	$database_user = 'measureit';
+	$database_passwd = 'measureitpasswd';
+	# email settings to send you emails when there a problems
+	$email_address = 'xxxx@gmail.com';
+	$email_smtp_server = 'smtp.google.com';
+	$email_smtp_user = 'xxxxxx';
+	$email_smtp_passwd = 'xxxxxx';
+	$email_smtp_tls = True;
+?>
