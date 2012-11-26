66abcd04957bd99bd3ad57f0cf88da0b3a4ec626
Thomas E <lalelunet@gmail.com>
2012-11-26 17:55:08 +0000

    add logging to file resolve mysql gone away problem

--- measureit_system_files/python/data-input.py
+++ measureit_system_files/python/data-input.py
@@ -9,6 +9,8 @@ import sys
 import platform
 import simplemail
 import urllib2
+import logging
+import traceback
 
 config = {}
 sensors = {}
@@ -20,59 +22,75 @@ err_critical = 0
 debug = False
 
 usbport = 'COM3'
+config_file_name = "C:\measureit\measureit.cfg.php"
+
 if platform.system() == 'Linux':
     usbport = '/dev/ttyUSB0'
+    config_file_name = "/usr/local/measureit/measureit.cfg.php"
+
+logger = logging.getLogger('MeasureIt')
+hdlr = logging.FileHandler('/tmp/measureit.log')
+formatter = logging.Formatter('%(asctime)s %(levelname)s %(message)s')
+hdlr.setFormatter(formatter)
+logger.addHandler(hdlr)
+
+if 'test' in sys.argv:
+    debug = True
+    logger.setLevel(logging.INFO)
+else:
+    logger.setLevel(logging.WARNING)
+
 
 def sensor_list_get():
     try:
-        db.execute('SELECT sensor_id FROM measure_sensors')
-        r = db.fetchall()
+        r = mysql_query('SELECT sensor_id FROM measure_sensors','fetchall')
         for row in r:
             sensor = int(row[0])
             sensors[sensor] = {'tmpr' : 0, 'watt' : 0}
         return sensors
     except:
-        error_handle('Error in sensor_list_get. No sensors or broken database connection?')
-        err_critical_count()
+        logger.error('Error in sensor_list_get. Error: '+traceback.format_exc())
 
 
 def system_settings_get():
-    db.execute('SELECT measure_system_setting_name, measure_system_setting_value FROM measure_system')
-    r = db.fetchall()
-    for row in r:
-        system_settings[row[0]] = row[1]
-    return True
+    try:
+        r = mysql_query('SELECT measure_system_setting_name, measure_system_setting_value FROM measure_system','fetchall')
+        for row in r:
+            system_settings[row[0]] = row[1]
+        return True
+    except:
+        logger.warning('Error in system_settings_get Error: '+traceback.format_exc())
 
 def sensor_data_change( coloum, sensor, data):
     try:
         sensor = str(sensor)
         data = str(data)
-        db.execute('UPDATE measure_data_now SET '+coloum+' = "'+data+'" WHERE sensor_id = '+sensor)
+        mysql_query('UPDATE measure_data_now SET '+coloum+' = "'+data+'" WHERE sensor_id = '+sensor)
     except:
-        error_handle('Error in sensor_data_change while try to change the current volt data')
+        logger.warning('Error in sensor_data_change while try to change the current volt data Error: '+traceback.format_exc())
         err_critical_count()
 
 def sensor_watt_insert( sensor, watt ):
     try:
         sensor = str(sensor)
         watt = str(watt)
-        db.execute('INSERT INTO measure_watt ( sensor, data, time) values( '+sensor+', '+watt+', NOW() ) ')
+        mysql_query('INSERT INTO measure_watt ( sensor, data, time) values( '+sensor+', '+watt+', NOW() ) ')
     except:
-        error_handle('Error in sensor_watt_insert while inserting watt into database')
+        logger.warning('Error in sensor_watt_insert while inserting watt into database Error: '+traceback.format_exc())
         err_critical_count()
 
 def tmpr_insert( tmpr ):
     try:
         now = datetime.datetime.now()
-        db.execute('INSERT INTO measure_tmpr ( data, time ) values( "'+str(tmpr)+'", NOW() ) ')
-        db.execute('INSERT IGNORE INTO measure_tmpr_hourly ( data, time, hour ) VALUES ( "'+str(tmpr)+'", NOW(), "'+str(now.hour)+'" )')
+        mysql_query('INSERT INTO measure_tmpr ( data, time ) values( "'+str(tmpr)+'", NOW() ) ')
+        mysql_query('INSERT IGNORE INTO measure_tmpr_hourly ( data, time, hour ) VALUES ( "'+str(tmpr)+'", NOW(), "'+str(now.hour)+'" )')
     
     except:
-        error_handle('Error in tmpr_insert while insert tmpr. Is there a connection to the database?')
-        err_critical_count()
+        logger.warning('Error in tmpr_insert while insert tmpr. Error: '+traceback.format_exc())
 
 def history_update( sensor, hist ):
     try:
+        mysql.ping(True)
         date_hour = date_hour_get(hist[1])
         if hist[0] == 'm':
             query = 'INSERT IGNORE INTO measure_watt_monthly ( sensor, data, time ) VALUES ( "'+str(sensor)+'", "'+str(hist[2])+'", "'+date_hour[0]+'" )'
@@ -80,9 +98,9 @@ def history_update( sensor, hist ):
             query = 'INSERT IGNORE INTO measure_watt_daily ( sensor, data, time ) VALUES ( "'+str(sensor)+'", "'+str(hist[2])+'", "'+date_hour[0]+'" )'
         if hist[0] == 'h':
             query = 'INSERT IGNORE INTO measure_watt_hourly ( sensor, data, hour, time ) VALUES ( "'+str(sensor)+'", "'+str(hist[2])+'", "'+date_hour[1]+'", "'+date_hour[0]+'" )'
-        db.execute(query)
+        mysql_query(query)
     except:
-        error_handle('Error in history_update. Is there a connection to the database?')
+        logger.warning('Error in history_update. Error: '+traceback.format_exc())
         err_critical_count()
 
 def cron_timer_hourly():
@@ -96,22 +114,21 @@ def cron_timer_hourly():
         day_from = datetime.date.today() - datetime.timedelta(days=1)
     date_from = str(day_from)+' '+str(hour_from)+':00:00'
     date_to = str(day_to)+' '+str(hour_to)+':00:00'
-    #try:
-    for sensor in sensors:
-        usage_sum_hourly = usage_sum_count = sum = 0
-        db.execute("select sensor, data from measure_watt where time between '"+date_from+"' AND '"+date_to+"' AND sensor="+str(sensor))
-        r = db.fetchall()
-        for row in r:
-            usage_sum_hourly += float(row[1])
-            usage_sum_count += 1
-        if usage_sum_count != 0:
-            sum = (usage_sum_hourly/usage_sum_count)/1000
-        query = 'INSERT IGNORE INTO measure_watt_hourly ( sensor, data, hour, time ) VALUES ( "'+str(sensor)+'", "'+str(sum)+'", "'+str(hour_from)+'", "'+str(day_from)+'" )'
-        db.execute(query)
-    usage_sum_hourly = usage_sum_count = sum = r = 0
-    #except:
-        #error_handle('Error in cron_timer_hourly. No sensors or broken database connection?')
-        #err_critical_count()
+    try:
+        for sensor in sensors:
+            usage_sum_hourly = usage_sum_count = sum = 0
+            r = mysql_query("select sensor, data from measure_watt where time between '"+date_from+"' AND '"+date_to+"' AND sensor="+str(sensor),'fetchall')
+            for row in r:
+                usage_sum_hourly += float(row[1])
+                usage_sum_count += 1
+            if usage_sum_count != 0:
+                sum = (usage_sum_hourly/usage_sum_count)/1000
+            query = 'INSERT IGNORE INTO measure_watt_hourly ( sensor, data, hour, time ) VALUES ( "'+str(sensor)+'", "'+str(sum)+'", "'+str(hour_from)+'", "'+str(day_from)+'" )'
+            mysql_query(query)
+        usage_sum_hourly = usage_sum_count = sum = r = 0
+    except:
+        logger.warning('Error in cron_timer_hourly. Error: '+traceback.format_exc())
+        err_critical_count()
         
     timer_hourly = threading.Timer(3600.0, cron_timer_hourly)
     timer_hourly.start()
@@ -126,8 +143,7 @@ def cron_timer_daily():
             usage_sum_daily = usage_sum_count = sum = 0
             date_from = str(datetime.date.today() - datetime.timedelta(days=1))+' 00:00:00'
             date_to = str(datetime.date.today())+' 00:00:00'
-            db.execute("select sensor, data from measure_watt where time between '"+date_from+"' AND '"+date_to+"' AND sensor="+str(sensor)) 
-            r = db.fetchall() 
+            r = mysql_query("select sensor, data from measure_watt where time between '"+date_from+"' AND '"+date_to+"' AND sensor="+str(sensor),'fetchall')
             try:
                 for row in r:
                     usage_sum_daily += float(row[1])
@@ -136,21 +152,21 @@ def cron_timer_daily():
                     sum = ((usage_sum_daily/usage_sum_count)*24)/1000
                     
                 query = 'INSERT IGNORE INTO measure_watt_daily ( sensor, data, time ) VALUES ( "'+str(sensor)+'", "'+str(sum)+'", "'+date_from+'" )'
-                db.execute(query)
+                mysql_query(query)
                 usage_sum_daily = usage_sum_count = sum = r = 0
             except:
-                error_handle('Error in cron_timer_daily. No sensors or broken database connection?')
+                logger.warning('Error in cron_timer_daily. Error: '+traceback.format_exc())
                 err_critical_count()
             try: # delete old watt data
                 if sensor_settings.has_key(sensor):
                     if sensor_settings[sensor] > 0:
                         query = 'DELETE FROM measure_watt WHERE sensor = '+str(sensor)+' AND time < NOW( ) - INTERVAL '+str(sensor_settings[sensor])+' DAY'
-                        db.execute(query)
+                        mysql_query(query)
             except:
-                error_handle('Error in cron_timer_hourly while deleting old data')
+                logger.warning('Error in cron_timer_hourly while deleting old data Error: '+traceback.format_exc())
                 err_critical_count()
     except:
-        error_handle('Error in cron_timer_daily. No sensors or broken database connection?')
+        logger.warning('Error in cron_timer_daily. Error: '+traceback.format_exc())
         err_critical_count()
         
 def cron_timer_weekly():
@@ -163,36 +179,35 @@ def update_check():
         nv = int(system_settings['current_version'])+1
         try:
             r = urllib2.urlopen('https://measureit.googlecode.com/files/measureit-'+int(nv)+'.zip')
-            db.execute('INSERT INTO measure_system ( measure_system_setting_name, measure_system_setting_value ) values ( "next_version", "'+str(nv)+'" )')
+            mysql_query('INSERT INTO measure_system ( measure_system_setting_name, measure_system_setting_value ) values ( "next_version", "'+str(nv)+'" )')
         except:
-            print 'Update: No new version found'
+            logger.info('Update: No new version found')
 
     else:
-        db.execute('INSERT INTO measure_system ( measure_system_setting_name, measure_system_setting_value ) values ( "current_version", 113 )')
+        mysql_query('INSERT INTO measure_system ( measure_system_setting_name, measure_system_setting_value ) values ( "current_version", 113 )')
 
 def system_settings_get():
-    db.execute('SELECT measure_system_setting_name, measure_system_setting_value FROM measure_system')
-    r = db.fetchall()
+    r = mysql_query('SELECT measure_system_setting_name, measure_system_setting_value FROM measure_system','fetchall')
     for row in r:
         system_settings[row[0]] = row[1]
     return True
 
 def sensor_settings_get():
-    db.execute('SELECT measure_sensor, measure_history FROM measure_settings')
-    r = db.fetchall()
-    for row in r:
-        sensor_settings[row[0]] = row[1]
-    return True
+    try:
+        r = mysql_query('SELECT measure_sensor, measure_history FROM measure_settings','fetchall')
+        for row in r:
+            sensor_settings[row[0]] = row[1]
+        return True
+    except:
+        logger.warning('Error in sensor_settings_get Error: '+traceback.format_exc())
                 
 def date_hour_get( hours ):
     try:
-        db.execute('SELECT NOW() - INTERVAL '+hours+' HOUR')
-        date = db.fetchone()
+        date = mysql_query('SELECT NOW() - INTERVAL '+hours+' HOUR','fetchone')
         r = re.search(r"(\d+-\d+-\d+) (\d+):.+", str(date[0]) )
         return (r.group(1), r.group(2))
     except:
-        error_handle('Error in data_hour_get. No sensors or broken database connection?')
-        err_critical_count()
+        logger.warning('Error in data_hour_get. Error: '+traceback.format_exc())
 
 def sensor_data_check( sensor, watt, tmpr ):
     sensor = int(sensor)
@@ -220,9 +235,6 @@ def mail_send(email_subject, email_body):
         use_tls = email_smtp_tls
     ).send()
 
-def error_handle(message):
-    print str(datetime.datetime.now())+' '+message
-
 def err_critical_count():
     global err_critical
     err_critical += 1
@@ -231,14 +243,13 @@ def err_critical_count():
             mail_send('Message from your measureit installation', 'Please take a look at your installation. It seems there is a problem...')
             err_critical = 0
         except:
-            error_handle('Can not send email. Please check your settings!')
+            logger.warning('Error in err_critical_count Can not send email. Please check your settings!')
 
 def config_parse():
-    file_name = "/usr/local/measureit/measureit.cfg.php"
     try:
-        config_file = open(file_name, 'r')
+        config_file = open(config_file_name, 'r')
     except:
-        print file_name+' could not be opened or read. Please check if file exists and that that the permissions are ok'
+        logger.error('Error in config_parse. '+config_file_name+' could not be opened or read. Please check if file exists and that that the permissions are ok Error: '+traceback.format_exc())
     
     for line in config_file:
         line = line.rstrip()
@@ -255,26 +266,39 @@ def config_parse():
                 config[r.group(1).rstrip()] = r.group(2).rstrip()
     return True
 
+def mysql_query(query, type = False):
+    if query:
+        try:
+            mysql = MySQLdb.connect(host=config['database_host'],port=int(config['database_port']),user=config['database_user'],passwd=config['database_passwd'],db=config['database_name'])
+            db = mysql.cursor()
+            
+            try:
+                if debug:
+                    logger.info('Try to execute query: '+query)
+                db.execute(query)
+                if type:
+                    if type == 'fetchone':
+                        return db.fetchone()
+                    if type == 'fetchall':
+                        return db.fetchall()
+                db.close()
+            except:
+                logger.error('Can not execute query. Error: '+traceback.format_exc())
+            
+        except:
+            logger.error('Can not connect to database. Is the database on and are the database settings ok? Error: '+traceback.format_exc())
+    
+    return True
+
 warnings.filterwarnings("ignore")
 
 try:
     ser = serial.Serial(port=usbport, baudrate=57600, bytesize=serial.EIGHTBITS, parity=serial.PARITY_NONE, stopbits=serial.STOPBITS_ONE, timeout=3)
 except:
-    error_handle('Can not connect to the com device. Please check the cable is plugged in and if the device has the correctly drivers installed')
-
-if 'test' in sys.argv:
-    debug = True
+    logger.error('Can not connect to the com device. Please check the cable is plugged in and if the device has the correctly drivers installed Error: '+traceback.format_exc())
 
 config_parse()
-
-try:
-    mysql = MySQLdb.connect(host=config['database_host'],port=int(config['database_port']),user=config['database_user'],passwd=config['database_passwd'],db=config['database_name'])
-    mysql.ping(True)
-    db = mysql.cursor()
-
-except:
-    error_handle('Can not connect to database. Is the database on and are the database settings ok?')
-
+#mysql_connect()
 
 sensors = sensor_list_get()
 system_settings_get()
@@ -327,3 +351,7 @@ while True:
             sensor_data_check( sensor, watt, tmpr )
            
         sensor_data_check( r.group(2), watt_sum, tmpr )
+
+
+
+            
