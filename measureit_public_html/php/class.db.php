497a4e7d366267c0b08e5a164eef156595e3e5fa
Thomas E <lalelunet@gmail.com>
2013-02-08 20:02:17 +0000

    change backup function to work with my pi
    todo: do it right for every system....

--- measureit_public_html/php/class.db.php
+++ measureit_public_html/php/class.db.php
@@ -39,8 +39,10 @@ class mydb{
 	}
 	
 	function backup(){
+		global $database_host, $database_name, $database_port, $database_user, $database_passwd;
 		set_time_limit(0);
-		system( 'mysqldump --opt -h'.$database_host.':'.$database_port.' -u'.$database_user.' -p'.$database_passwd.' '.$database_name.' | gzip > ../backup/measureit_backup_'.date('Ymd-His').'.gz' );
+		#var_dump('mysqldump --opt -h'.$database_host.':'.$database_port.' -u'.$database_user.' -p'.$database_passwd.' '.$database_name.' | gzip > ../backup/measureit_backup_'.@date('Ymd-His').'.gz');
+		system( 'mysqldump --opt -h'.$database_host.' -u'.$database_user.' -p'.$database_passwd.' '.$database_name.' | gzip > ../backup/measureit_backup_'.date('Ymd-His').'.gz' );
 	}
 
 }
